1121abf1e1b94cb2d5f5601e9e685bbcaf8a0123
Matt Horridge <matthorridge@gmail.com>
2019-06-16 13:19:37 +1000

    reformat

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import SearchBar from './SearchBar';
+import axios from 'axios';
 
-const App =  () => {
-    return <div className="ui container" style={{marginTop: '10px'}}>
-        <SearchBar></SearchBar>
+class App extends React.Component {
+    state = { images:[] };
+
+     onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos',{
+            params:{
+                query: term
+            },
+            headers: {
+                Authorization: 'Client-ID 8bc89a78133c938bf64d755a65109938d15d3c798b426272726f7a6d6e44ff74'
+            }
+        });
+
+       this.setState({images: response.data.results});
+    }
+
+
+    render () {
+    return (<div className="ui container" style={{marginTop: '10px'}}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
     </div>
+    );
+    }
 }
 
 export default App;
