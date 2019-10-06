9362e1f1323f11bd4a0ea9c97c35ef18a66316d5
DESKTOP-TRVU284\Matt <matthorridge@gmail.com>
2019-10-06 16:03:14 +0100

    more redux work

--- nlog/src/actions/index.js
+++ nlog/src/actions/index.js
@@ -1,15 +1,61 @@
 import jsonPlaceHolder from '../apis/jsonPlaceHolder'
+import _ from 'lodash';
+
+
+
+export const fetchPostsAndUsers = () => {
+    return async (dispatch, getState) => {
+        await dispatch(fetchPosts());
+        const userIds =_.uniq(_.map(getState().posts, 'userId'));
+        userIds.forEach(id => dispatch(fetchUser(id)));
+    }
+}
+
+
 export const fetchPosts = () => {
 
     return async (dispatch) => {
         const response = await jsonPlaceHolder.get('/posts');
-        const act = () => {
-            return {type: 'FETCH_POSTS', payload: response}
+        const action = () => {
+            return {type: 'FETCH_POSTS', payload: response.data}
         }
-        dispatch(act())
+        dispatch(action())
     }
 
   
 
 };
 
+export const fetchUser = (id) => {
+    return async (dispatch) => {
+       
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+    const action = () => {
+        return {type: 'FETCH_USER', payload: response.data}
+    }
+    dispatch(action())
+    }
+};
+
+
+
+
+
+//Memoise fetch
+/*
+export const fetchUser = (id) => {
+    return (dispatch) => {
+        _fetchUser(id, dispatch);
+    }
+};
+
+//Memoise fetch
+const  _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+        const action = () => {
+            return {type: 'FETCH_USER', payload: response.data}
+        }
+        dispatch(action())
+});
+
+*/
