eb195cf8e4d08609c21ad0fa8c3f6894c04618b6
DESKTOP-TRVU284\Matt <matthorridge@gmail.com>
2019-10-13 21:42:42 +0100

    more redux work

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,8 +1,7 @@
 import React from 'react'
-
+import {connect} from 'react-redux';
+import {signIn, signOut} from '../actions';
 class GoogleAuth extends React.Component{
-
-    state = {isSignedIn : null};
 
     componentDidMount(){
         window.gapi.load('client:auth2', () =>{
@@ -11,24 +10,29 @@ class GoogleAuth extends React.Component{
                 scope: 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({isSignedIn: this.auth.isSignedIn.get()});
+                this.onAuthChange(this.auth.isSignedIn.get());
                 this.auth.isSignedIn.listen(this.onAuthChange);
             });
         })
     }
 
-    onAuthChange = () =>{
-        this.setState({isSignedIn : this.auth.isSignedIn.get()});
+    onAuthChange = (isSignedIn) =>{
+        if(isSignedIn){
+            this.props.signIn(this.auth.currentUser.get().getId());
+        }
+        else {
+            this.props.signOut();
+        }
     };
 
 
     renderAuthButton(){
-        if(this.state.isSignedIn === null){
+        if(this.props.isSignedIn === null){
             return null;
         }
-        else if(this.state.isSignedIn){
+        else if(this.props.isSignedIn){
             return (
-                <button onClick={this.onSignOut} className="ui red google button">
+                <button onClick={this.onSignOutClick} className="ui red google button">
                     <i className="google icon"></i>
                     Sign Out
                 </button>
@@ -36,7 +40,7 @@ class GoogleAuth extends React.Component{
         }
         else {
             return (
-                <button onClick={this.onSignIn} className= "ui red google button">
+                <button onClick={this.onSignInClick} className= "ui red google button">
                    <i className="google icon"></i>     
                     Sign In With Google
                 </button>
@@ -47,12 +51,12 @@ class GoogleAuth extends React.Component{
 
 
 
-    onSignIn = () => {
+    onSignInClick = () => {
         this.auth.signIn();
     };
 
 
-    onSignOut = () => {
+    onSignOutClick = () => {
         this.auth.signOut();
     };
 
@@ -62,4 +66,12 @@ class GoogleAuth extends React.Component{
     }
 }
 
-export default GoogleAuth;
+const mapStateToProps = (state) => {
+    return {isSignedIn: state.auth.isSignedIn};
+}
+
+
+export default connect(
+    mapStateToProps, 
+    {signIn, signOut})
+    (GoogleAuth);
--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,8 +1,70 @@
-import React from 'react'
+import React from 'react';
+import {Field, reduxForm} from 'redux-form';
+import {connect } from 'react-redux';
+import {createStream} from '../../actions';
+
+class StreamCreate extends React.Component{
+
+    renderError({error, touched}){
+        if(touched && error){
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            );
+        }
+    }
 
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>
+    renderInput = (formProps) => {
+     const className = `field ${formProps.meta.error && formProps.meta.touched ? 'error' : ''}`
+      return (
+        <div className={className}>
+            <label>{formProps.label}</label>
+            <input {...formProps.input}/>
+            {this.renderError(formProps.meta)}
+        </div>
+      );    
+    }
+
+
+    onSubmit = (formValues) =>{
+        this.props.createStream(formValues);
+    }
+
+    render(){
+        
+        return ( 
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+                <Field name="title" component={this.renderInput} label="Enter Title"/>
+                <Field name="description" component={this.renderInput} label="Enter Description"></Field>
+                <button className="ui button primary">Submit</button>
+            </form>
+
+        );
+    }
 }
 
-export default StreamCreate;
+
+const validate = (formValues) => {
+    const errors = {};
+
+    if(!formValues.title){
+        errors.title = "Must enter a title";
+    }
+
+    if(!formValues.description){
+        errors.description = "Must enter description";
+    }
+
+    return errors;
+}
+
+
+
+const formWrapped = reduxForm({
+    form: 'streamCreate',
+    validate
+})(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped);
