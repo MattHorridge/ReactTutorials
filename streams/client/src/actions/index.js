abe0c2a7bffd55b880cd340551232c87c3f42ef2
DESKTOP-TRVU284\Matt <matthorridge@gmail.com>
2019-10-17 07:41:36 +0100

    more REST work

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -24,8 +24,9 @@ export const signOut = () => {
 
 
 ///Passing in the dispatch function from thunk as a variable 
-export const createStream = (formValues) =>  async (dispatch) => {
-    const response =   await streams.post('/streams', formValues);
+export const createStream = (formValues) =>  async (dispatch, getState) => {
+    const {userId} = getState().auth;
+    const response =   await streams.post('/streams', { ...formValues, userId});
     dispatch({type: CREATE_STREAM, payload: response.data})
 };
 
@@ -39,8 +40,6 @@ export const fetchStream = (id) => async dispatch => {
     const response = await streams.get(`/streams/${id}`);
     dispatch({type: FETCH_STREAM, payload: response.data})
 }
-
-
 
 export const deleteStream = (id) => async dispatch => {
    await streams.delete(`/streams/${id}`);
